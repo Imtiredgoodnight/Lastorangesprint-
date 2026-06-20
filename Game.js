10c4d1ef1222d61b43982b39b60ce804ec6183f5
imtiredgoodnight <1081544@lwsd.org>
2026-06-19 19:36:40 -0700

    Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- Game.js
+++ Game.js
@@ -7,5 +7,32 @@ const songs = [
     "Golds Gone",
     "Lose You To Love Me",
     "Someone You Loved",
-    "Drivers License"
+    "Drivers License",
+    "Petty"
 ]
+
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", function(){
+   const answers = document.querySelectorAll(".answer");
+   var score = 0
+
+    for (let i = 0; i < 10: i++) {
+        var correctAnswer = songs[i].toLowerCase();
+        var userAnswer = answers[i].value.trim().toLowerCase();
+
+        var result = document.createElement("p")
+
+        if (userAnswer == correctAnswer) {
+            result.textContent("✅");
+            score += 100
+        } else {
+            result.textContent("❌Your a failure!")
+        }
+
+        answers[i].parentElement.appendChild(result)
+    }
+    const scoreDisplay = document.createElement("h2");
+    scoreDisplay.textContent = "Score: " + score;
+    document.body.appendChild(scoreDisplay);
+}
+);
